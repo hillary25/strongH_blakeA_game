aae321851cb4b5335469dfb8b3bc859975d11a00
hillary25 <35806842+hillary25@users.noreply.github.com>
2018-04-18 15:43:07 -0400

    April 18

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 	//Variables
 	//Array of Cards
-	const cards = [{
+	var cards = [{
 		'name': 'badge',
 		'img': 'images/badge.svg',
 		},
@@ -47,10 +47,11 @@
 		count = 0,
 		previousTarget = null,
 		delay = 1200,
-		score = 15,
+		score = 0,
 		resetScreen = document.querySelector('.reset-screen'),
 		resetButton = resetScreen.querySelector('button');
 
+    //Shuffle cards
 	gameGrid.sort(() => 0.5 - Math.random());
 
 	//Card Creation Loop
@@ -81,6 +82,10 @@
 		selected.forEach(card => {
 			card.classList.add('match');
 			score ++;
+			if (score === 16) {
+                //debugger;
+				showResetScreen();
+			}
 			document.querySelector('h4').textContent = `Score: ${score}`;
 		});
 	}
@@ -123,13 +128,10 @@
 			if (firstGuess !== '' && secondGuess !== '') {
 				//and if they match
 				if (firstGuess === secondGuess) {
-					if (score === 16) {
-						debugger;
-						showResetScreen();
-					} else {
+
 						//run the match function and flip the cards
 						setTimeout(match, delay);
-					}
+					} else {
 				}
 					//otherwise reset the guesses and flip the cards
 					setTimeout(resetGuesses, delay);
@@ -142,12 +144,115 @@
 	function showResetScreen(message) {
 		//user has won, reset stuff and start game over
 		console.log('you win');
-		resetScreen.classList.add('.show-screen');
-		resetScreen.querySelector('h3').textContent = message;
+		resetScreen.classList.add('show-screen');
+		//resetScreen.querySelector('h3').textContent = message;
 	}
+
+	function resetGame() {
+		score = 0;
+		document.querySelector('h4').textContent = `Score: ${score}`;
+
+		firstGuess = '';
+		secondGuess = '';
+		count = 0;
+		previousTarget = null;
+
+		var selected = document.querySelectorAll('.selected');
+		selected.forEach(card => {
+			card.classList.remove('selected');
+		});
+
+		var selected = document.querySelectorAll('.match');
+		selected.forEach(card => {
+			card.classList.remove('match');
+		});
+
+		resetScreen.classList.remove('show-screen');
+		cards=[];
+		init();
+	}
+
+	//remove, shuffle, put back in
+	function init() {
+    //debugger;
+		if (cards.length == 0) {
+		cards = [{
+			'name': 'badge',
+			'img': 'images/badge.svg',
+			},
+			{
+				'name': 'helmet',
+				'img': 'images/helmet.svg',
+			},
+			{
+				'name': 'gun',
+				'img': 'images/gun.svg',
+			},
+			{
+				'name': 'ship',
+				'img': 'images/ship.svg',
+			},
+			{
+				'name': 'star',
+				'img': 'images/star.svg',
+			},
+			{
+				'name': 'title',
+				'img': 'images/title.svg',
+			},
+			{
+				'name': 'planet',
+				'img': 'images/planet.svg',
+			},
+			{
+				'name': 'trooper',
+				'img': 'images/trooper.svg',
+			},
+		];
+	}
+
+		while (gameGrid.firstChild) {
+			grid.removeChild(card.firstChild);
+			card.removeChild(front.firstChild);
+			card.removeChild(back.firstChild);
+		}
+
+		// gameGrid.forEach(card => {
+		// 	grid.removeChild(card);
+		// 	card.removeChild(front);
+		// 	card.removeChild(back);
+		// 	});
+
+		//shuffle cards over
+		gameGrid.sort(() => 0.5 - Math.random());
+
+		gameGrid.forEach(item => {
+			const {name, img} = item;
+
+			const card = document.createElement('div');
+			card.classList.add('card');
+			card.dataset.name = name;
+
+			//front of cards
+			const front = document.createElement('div');
+			front.classList.add('front');
+
+			//back of cards (with image)
+			const back = document.createElement('div');
+			back.classList.add('back');
+			back.style.backgroundImage = `url(${item.img})`;
+
+			grid.appendChild(card);
+			card.appendChild(front);
+			card.appendChild(back);
+		});
+	}
+
 
 
 	//Event Handling
 	grid.addEventListener('click', selectCard);
-	//resetButton.addEventListener('click', resetGame);
+	resetButton.addEventListener('click', resetGame);
+
+	init();
 })();
